f906d658097b034ac195c48b038937734bb1f186
emanuels8 <emanuels8001@gmail.com>
2025-02-19 05:36:10 -0600

    Removed dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-import dotenv from "dotenv";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-
-  dotenv.config();
-
   return {
     base: "./",
     define: {
