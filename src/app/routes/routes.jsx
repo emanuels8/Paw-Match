8974a8f7eb2afa4d2105f35a77d662527189692f
emanuels8 <emanuels8001@gmail.com>
2025-02-20 00:51:43 -0600

    Default route fixes

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -1,4 +1,3 @@
-import { DogSearch } from "../../features/dogs/components/DogSearch";
 import { DogsLayout } from "../../features/dogs/components/layout/DogsLayout";
 import { ViewFavoriteDog } from "../../features/dogs/components/ViewFavoriteDog";
 import { Login } from "./auth/Login";
@@ -6,21 +5,12 @@ import { Login } from "./auth/Login";
 const routes = {
   privateRoutes: [
     {
-      path: "*",
-      element: DogsLayout,
-    },
-    {
-      path: "/dogs",
-      element: DogsLayout,
-    },
-    {
-      path: "/dogs/search",
-      element: DogSearch,
-    },
-    {
       path: "/dogs/favorites/:dogId",
       element: ViewFavoriteDog,
-      featureName: "Tickets",
+    },
+    {
+      path: "*",
+      element: DogsLayout,
     },
   ],
   publicRoutes: [
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: "./",
+    base: "/",
     define: {
       "import.meta.env": JSON.stringify(env),
     },
